6436123fa9d44e1f998d84e1ab66378df5f75ca8
caixibei <18851037162@139.com>
2024-12-27 12:33:39 +0800

    代码初始化

--- src/main/java/tsai/spring/boot/TsaiMonitorApplication.java
+++ src/main/java/tsai/spring/boot/TsaiMonitorApplication.java
@@ -1,11 +1,22 @@
 package tsai.spring.boot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
 @SpringBootApplication
 public class TsaiMonitorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TsaiMonitorApplication.class, args);
-    }
+    private static final Logger logger = LoggerFactory.getLogger(TsaiMonitorApplication.class);
 
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(TsaiMonitorApplication.class, args);
+        Environment environment = context.getEnvironment();
+        String port = environment.getProperty("server.port");
+        logger.info("====================服务运行环境信息=====================");
+        logger.info("服务端口：{}",port);
+        logger.info("=======================================================");
+    }
 }
