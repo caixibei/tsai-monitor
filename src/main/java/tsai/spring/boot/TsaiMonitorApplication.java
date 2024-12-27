00d094cb529b73365eff36eedeaa8ad966bbc8da
caixibei <18851037162@139.com>
2024-12-27 12:26:47 +0800

    代码初始化

---
+++ src/main/java/tsai/spring/boot/TsaiMonitorApplication.java
@@ -0,0 +1,11 @@
+package tsai.spring.boot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class TsaiMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsaiMonitorApplication.class, args);
+    }
+
+}
